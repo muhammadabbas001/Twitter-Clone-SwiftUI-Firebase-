069bfd1f297c15a1a0c1a0b386146094967b89b8
Muhammad Abbas <73989872+muhammadabbas001@users.noreply.github.com>
2022-02-12 09:59:41 +0500

    Initial Commit

---
+++ TwitterClone/TwitterCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterCloneApp.swift
+//  TwitterClone
+//
+//  Created by Muhammad Abbas on 12/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct TwitterCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
